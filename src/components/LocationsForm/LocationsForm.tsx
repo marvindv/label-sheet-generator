4e693223cd5f4267489c4e072ccd8570be199f05
Marvin Davieds <marvin.davieds@gmail.com>
2024-11-05 21:26:05 +0100

    refactor(LocationsForm): replace react-hook-form with @mantine/form

--- src/components/LocationsForm/LocationsForm.tsx
+++ src/components/LocationsForm/LocationsForm.tsx
@@ -1,102 +1,96 @@
-import { useEffect } from 'react';
 import { IconCopy, IconPlus, IconTrash } from '@tabler/icons-react';
-import { useFieldArray, useForm, useWatch } from 'react-hook-form';
 import { ActionIcon, Button, Group, Textarea, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { randomId } from '@mantine/hooks';
 
 export interface Props {
-  defaultValue: FormValues['location'];
-  onValueChange: (newValue: FormValues['location']) => void;
+  defaultValue: FormValues['locations'];
+  onValueChange: (newValue: FormValues['locations']) => void;
 }
 
 export type LocationsFormValue = {
   description?: string;
   url?: string;
+  randomId: string;
 }[];
 
 type FormValues = {
-  location?: LocationsFormValue;
+  locations?: LocationsFormValue;
 };
 
 export function LocationsForm(props: Props) {
-  const {
-    register,
-    control,
-    formState: { errors },
-  } = useForm<FormValues>({
-    defaultValues: {
-      location: props.defaultValue,
+  const form = useForm<FormValues>({
+    mode: 'uncontrolled',
+    initialValues: {
+      locations: props.defaultValue,
     },
-    mode: 'onBlur',
+    onValuesChange(values) {
+      props.onValueChange(values.locations);
+    },
   });
-  const formValue = useWatch({ control });
-  // TODO: There is probably a more elegant way without an effect.
-  useEffect(() => {
-    props.onValueChange(formValue.location);
-  }, [formValue]);
-  const { fields, append, remove, insert } = useFieldArray({
-    name: 'location',
-    control,
-  });
+
+  const fields = form.getValues().locations?.map((item, index) => (
+    <Group key={index}>
+      <Textarea
+        flex={1}
+        label="Description"
+        placeholder="Description"
+        key={form.key(`locations.${index}.description`)}
+        {...form.getInputProps(`locations.${index}.description`)}
+        autosize
+      />
+      <TextInput
+        flex={1}
+        style={{ alignSelf: 'end' }}
+        label="URL"
+        placeholder="URL"
+        key={form.key(`locations.${index}.url`)}
+        {...form.getInputProps(`locations.${index}.url`)}
+      />
+      <ActionIcon
+        variant="light"
+        aria-label="Duplicate"
+        onClick={() =>
+          form.insertListItem(
+            'locations',
+            {
+              description: form.getValues().locations?.[index].description,
+              url: form.getValues().locations?.[index].url,
+              randomId: randomId(),
+            },
+            index + 1
+          )
+        }
+        style={{ alignSelf: 'end' }}
+        size="lg"
+      >
+        <IconCopy />
+      </ActionIcon>
+      <ActionIcon
+        variant="light"
+        color="red"
+        aria-label="Trash"
+        onClick={() => form.removeListItem('locations', index)}
+        style={{ alignSelf: 'end' }}
+        size="lg"
+      >
+        <IconTrash />
+      </ActionIcon>
+    </Group>
+  ));
+
   return (
     <form>
-      {fields.map((field, index) => {
-        return (
-          <Group key={field.id}>
-            <Textarea
-              flex={1}
-              label="Description"
-              placeholder="Description"
-              {...register(`location.${index}.description` as const, {
-                required: true,
-              })}
-              className={errors?.location?.[index]?.description ? 'error' : ''}
-              autosize
-            />
-            <TextInput
-              flex={1}
-              style={{ alignSelf: 'end' }}
-              label="URL"
-              placeholder="URL"
-              {...register(`location.${index}.url` as const, {
-                required: true,
-              })}
-              className={errors?.location?.[index]?.url ? 'error' : ''}
-            />
-            <ActionIcon
-              variant="light"
-              aria-label="Duplicate"
-              onClick={() =>
-                insert(index + 1, {
-                  description: formValue.location?.[index].description,
-                  url: formValue.location?.[index].url,
-                })
-              }
-              style={{ alignSelf: 'end' }}
-              size="lg"
-            >
-              <IconCopy />
-            </ActionIcon>
-            <ActionIcon
-              variant="light"
-              color="red"
-              aria-label="Trash"
-              onClick={() => remove(index)}
-              style={{ alignSelf: 'end' }}
-              size="lg"
-            >
-              <IconTrash />
-            </ActionIcon>
-          </Group>
-        );
-      })}
+      {fields}
 
       <Button
         variant="light"
         mt="md"
         onClick={() =>
-          append({
+          form.insertListItem('locations', {
             description: '',
             url: '',
+            randomId: randomId(),
           })
         }
         leftSection={<IconPlus />}
